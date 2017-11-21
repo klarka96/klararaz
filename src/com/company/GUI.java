88761879369719ef6bb1f681d0159d86e20ef91a
dani <daniel.kac94@gmail.com>
2017-11-22 00:32:02 +0100

    stworzylem plik GUI

--- src/com/company/GUI.java
+++ src/com/company/GUI.java
@@ -3,12 +3,13 @@ package com.company;
 import javax.swing.*;
 
 public class GUI extends JFrame{
-// lubie kaszztany
+// lubie kaszztany daj komit
         public static void main(String[] args) {
             GUI mainGui = new GUI();
             mainGui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             mainGui.setSize(800,600);
             mainGui.setTitle("Synthia v.1.0");
+
             mainGui.setVisible(true);
         }
 }
